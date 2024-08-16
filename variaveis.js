8e6ec4308a6ba892cbafc416444fd9ead583e5b5
0tt0ni <filypeottoni@gmail.com>
2024-08-16 10:33:26 -0300

    aula 2

---
+++ variaveis.js
@@ -0,0 +1,27 @@
+const i = 1;
+if(i == 1){
+
+    const n =3;
+}
+
+var j = 3;
+j = 2;
+var j;
+
+if(true){
+    var x = 7;
+}
+console.log(x);
+
+let z = 3;
+
+
+minhaStringA = 'texto 1';
+minhaStringB = "texto '2";
+minhaStringC = "texto $(z)";
+
+let n1 = 1;
+let n2 = 5;
+let n3 = '1.0' //tipagem errada
+
+console.log(minhaStringB);
