c3b9b3a974950dc20b5ef42f5a85a6a577c5a800
0tt0ni <111684934+0tt0ni@users.noreply.github.com>
2024-08-23 09:51:40 -0300

    Add files via upload
    solução atividade um programação web 2024

---
+++ atividade1/atividade1.js
@@ -0,0 +1,19 @@
+let x = 2;
+
+let y = 3;
+
+let z;
+
+
+
+z = x + y;
+    console.log("O resultado da soma entre "+ x +" e "+ y +" é "+ z);
+
+z = x - y;
+    console.log(`O resultado da subtração entre ${x} e ${y} é ${z}`);
+
+z = x * y;
+    console.log(`O resultado da multiplicação entre ${x} e ${y} é ${z}`);
+
+z = x / y;
+    console.log(`O resultado da divisão entre ${x} e ${y} é ${z}`);
